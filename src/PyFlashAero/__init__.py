eab23f29e60c34606e564c2d9989fbdf6c386735
Christian Holl <choll@synapticon.com>
2014-01-12 14:39:47 +0100

    add image viewer

--- src/PyFlashAero/__init__.py
+++ src/PyFlashAero/__init__.py
@@ -6,10 +6,24 @@ Licence: GNU AGPL
 @author: Christian Holl
 '''
 from PyFlashAero import flashair
+from PyFlashAero import ImageViewer
+from PyQt4 import QtGui
+
+# if __name__ == '__main__':
+#     print("PyFlashAero")
+#     a=flashair.connection('192.168.0.16', 80,1000)
+#     print(a.send_command(flashair.command.Get_file_list,directory='/'))
+#     while True:
+#         a.sync_new_pictures_since_start('/DCIM/101EOS5D', '/home/cyborg-x1/dwhelper')
+#         pass
+
+
 
 if __name__ == '__main__':
-    a=flashair.connection('192.168.0.16', 80,1000)
-    #print(a.send_command(flashair.command.Get_file_list,directory='/'))
-    while True:
-        a.sync_new_pictures_since_start('/DCIM/101EOS5D', '/home/cyborg-x1/dwhelper')
-        pass
+ 
+    import sys
+ 
+    app = QtGui.QApplication(sys.argv)
+    imageViewer = ImageViewer.ImageViewer()
+    imageViewer.show()
+    sys.exit(app.exec_())
